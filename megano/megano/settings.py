bc10a3f9e37970ccfe7ab2ba123f59db6bc7e04a
Fess <maxzavalov@gmail.com>
2023-07-10 17:55:21 +0400

    add model Avatar

--- megano/megano/settings.py
+++ megano/megano/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'my_auth.apps.MyAuthConfig',
+    'frontend',
+
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
--- megano/my_auth/models.py
+++ megano/my_auth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Avatar(models.Model):
+    """ Модель для хранения аватара пользователя"""
+
+    src = models.ImageField(upload_to='my_auth/avatars/user_avatars',
+                            default='my_auth/avatars/default.png',
+                            verbose_name='Ссылка')
+    alt = models.CharField(max_length=128, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = "Аватар"
+        verbose_name_plural = "Аватары"
+
