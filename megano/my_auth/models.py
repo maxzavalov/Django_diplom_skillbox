1a61e6be80116530bcadd5f67b231d82c5cfc890
Fess <maxzavalov@gmail.com>
2023-07-10 18:21:41 +0400

    add model Profile

--- megano/my_auth/models.py
+++ megano/my_auth/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,3 +14,12 @@ class Avatar(models.Model):
         verbose_name = "Аватар"
         verbose_name_plural = "Аватары"
 
+
+class Profile(models.Model):
+    """Модель профиля пользователя"""
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    fullName = models.CharField(max_length=128, verbose_name='Полное имя')
+    phone = models.PositiveIntegerField(blank=True, null=True, unique=True, verbose_name='Номер телефона')
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default=0, verbose_name='Баланс')
+    avatar = models.ForeignKey(Avatar, on_delete=models.CASCADE, related_name='profile', verbose_name='Аватар')
