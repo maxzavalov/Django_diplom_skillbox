8f9581c6d259c42cb0f0865004bf9312e9e6a1ca
Fess <maxzavalov@gmail.com>
2023-07-10 18:40:04 +0400

    add SingInView

--- megano/my_auth/views.py
+++ megano/my_auth/views.py
@@ -1,3 +1,24 @@
+import json
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class SingInView(APIView):
+
+    def post(self, request) -> Response:
+        serialized_data = list(request.POST.keys())[0]
+        user_data = json.loads(serialized_data)
+        username = user_data.get("username")
+        password = user_data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response(
+                status=status.HTTP_201_CREATED)
+
+        return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
